4b3d28ef201e2121e990be2c573b589e9612bdbb
Hyok An <anhyok@gmail.com>
2023-03-06 22:47:50 +0900

    ch01 마지막 업데이트

--- chap01/src/sec03/exam02/RunStatementExample.java
+++ chap01/src/sec03/exam02/RunStatementExample.java
@@ -4,10 +4,12 @@ public class RunStatementExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int x = 1;
-		int y = 2;
-		int result = x + y;
-		System.out.println(result);
+		int x;						// 변수 x를 선언
+		x = 1;						// 변수 x를 선언하고 1을 저장
+		int y = 2;					// 변수 y를 선언하고 2를 저장
+		int result = x + y;			/* 변수 result를 선언하고
+										변수 x와 y를 더한 값을 저장 */
+		System.out.println(result);	// println 메소드 호출
 	}
 
 }
