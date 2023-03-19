4632fbdab69e443d5cafffeb87dd80c532c2d94a
Hyok An <anhyok@gmail.com>
2023-03-20 08:32:31 +0900

    update

--- chap05/src/sec03/verify/exam02/Exercise13.java
+++ chap05/src/sec03/verify/exam02/Exercise13.java
@@ -4,8 +4,11 @@ public class Exercise13 {
 	public static void main(String[] args) {
 		LoginResult result = LoginResult.FAIL_PASSWORD;
 		if(result == LoginResult.SUCCESS) {
+			System.out.println("Login Success!");
 		} else if(result == LoginResult.FAIL_ID) {
+			System.out.println("FAIL: Wrong ID");
 		} else if(result == LoginResult.FAIL_PASSWORD) {
+			System.out.println("FAIL: Wrong Password");
 		}
 	}
 }
