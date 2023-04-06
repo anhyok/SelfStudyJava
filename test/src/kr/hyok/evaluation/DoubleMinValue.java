4201c918d665c4dfb35968cda532e7c56a1349dc
Hyok An <anhyok@gmail.com>
2023-04-07 08:53:08 +0900

    .

---
+++ test/src/kr/hyok/evaluation/DoubleMinValue.java
@@ -0,0 +1,13 @@
+package kr.hyok.evaluation;
+
+public class DoubleMinValue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double dMin = Double.MIN_VALUE;
+		System.out.println(dMin);
+		System.out.println(dMin*5001/10000);
+		System.out.println(dMin*5000/10000);
+	}
+
+}
